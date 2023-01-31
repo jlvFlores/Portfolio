9003ab5aa6db7c62501200cac4d0b83eb061be02
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2023-01-30 19:42:21 -0600

    Fix JS script linter errors

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let screenSize = window.matchMedia("(max-width: 768px)")
+const screenSize = window.matchMedia('(max-width: 768px)');
 const navMenuListItems = document.querySelectorAll('nav > *');
 const header = document.querySelector('header');
 const logo = document.querySelector('#logo');
 
-if (screenSize.matches) {
-    navMenuListItems.forEach(element => element.addEventListener('click', toggleMenu));
+function toggleMenu() {
+  logo.classList.toggle('hide');
+  header.classList.toggle('header-container');
+  navMenuListItems.forEach((element) => element.classList.toggle('hide'));
 }
 
-function toggleMenu() {
-    logo.classList.toggle('hide');
-    header.classList.toggle('header-container');
-    navMenuListItems.forEach(element => element.classList.toggle('hide'));
+if (screenSize.matches) {
+  navMenuListItems.forEach((element) => element.addEventListener('click', toggleMenu));
 }
