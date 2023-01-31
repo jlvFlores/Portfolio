92d71617f149a77cfa61bba53454c00212f46b13
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2023-01-31 10:31:28 -0600

    Fix js menu bug

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const screenSize = window.matchMedia('(max-width: 768px)');
+const screenSize = window.matchMedia('(max-width: 767px)');
 const navMenuListItems = document.querySelectorAll('nav > *');
 const header = document.querySelector('header');
 const logo = document.querySelector('#logo');
 
 function toggleMenu() {
-  logo.classList.toggle('hide');
-  header.classList.toggle('header-container');
-  navMenuListItems.forEach((element) => element.classList.toggle('hide'));
+  if (screenSize.matches) {
+    logo.classList.toggle('hide');
+    header.classList.toggle('header-container');
+    navMenuListItems.forEach((element) => element.classList.toggle('hide'));
+  }
 }
 
-if (screenSize.matches) {
-  navMenuListItems.forEach((element) => element.addEventListener('click', toggleMenu));
-}
+navMenuListItems.forEach((element) => element.addEventListener('click', toggleMenu));
